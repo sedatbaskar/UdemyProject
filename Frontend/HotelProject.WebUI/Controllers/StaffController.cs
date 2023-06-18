11a9ad735971aa2c125811db999cb819c09f9bf6
Sedat Başkar <sedatbaskar@gmail.com>
2023-06-18 20:43:33 +0300

    Lesson 22 Staff için Api Consume Edildi

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,18 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class StaffController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StaffController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient(); //istemci
+            var responseMessage = await client.GetAsync("http://localhost:51109/api/Staff"); //istek aldığımzı adres
+            if (responseMessage.IsSuccessStatusCode) //gelen veri başarılıysa
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();//gelen json veriyi dönüştürdüm
+                var values = JsonConvert.DeserializeObject<List<StaffViwModel>>(jsonData);
+                return View(values); 
+            }
             return View();
         }
 
-        
+
     }
 }
